7535b99116fc06ce3d18cd368c53ffe7e0a7fe4c
Yogesh K Singh <yogesh696ksingh@gmail.com>
2017-09-06 09:43:22 +0530

    initial commit

---
+++ resources/views/includes/links.blade.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Auction | Welcome</title>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<script src="js/vendor/modernizr.js"></script>
+<link rel="stylesheet" href="../library/foundation/css/foundation.min.css">
+<link rel="stylesheet" type="text/css" href="css/custom.css">
+<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+<!-- <script type="text/javascript" src="../library/materialize/js/materialize.min.js"></script> -->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@include('includes.links')
+	<title>Auction | Welcome</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
